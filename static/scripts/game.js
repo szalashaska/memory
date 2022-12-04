cd766895738c3355672e5da988be742384fadb15
szalashaska <kamilpetryniak@gmail.com>
2022-12-04 13:07:22 +0100

    Game styling

--- static/scripts/game.js
+++ static/scripts/game.js
@@ -1,1 +1,28 @@
-console.log("hello");
+// Because of Jinja syntax, const pairs must have been assigned in game.html.
+
+// <script type="text/javascript">
+//   const pairs = "{{ pairs }}"
+// </script>
+const gameCardContainer = document.querySelector(".game-cards-container");
+
+const assignClassAccordingToPairs = (pairsCount) => {
+  switch (pairsCount) {
+    case "6":
+      gameCardContainer.classList.add("pairs6");
+      break;
+    case "12":
+      gameCardContainer.classList.add("pairs12");
+      break;
+    case "18":
+      gameCardContainer.classList.add("pairs18");
+      break;
+    default:
+      console.log("Wrong pairs number");
+  }
+};
+
+const init = () => {
+  assignClassAccordingToPairs(pairs);
+};
+
+init();
